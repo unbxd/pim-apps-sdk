a853425299a80dd184fa241e4f648bfd5d112a45
Aninda-K <130359361+Aninda-K@users.noreply.github.com>
2023-08-25 16:57:02 +0530

    Update utils.py

--- src/pim_apps/utils.py
+++ src/pim_apps/utils.py
@@ -342,7 +342,15 @@ class Dict2Class(object):
 
 def slack_notifier(channel="#infinity-template-jobs", title="Pepper-X App Alert",
                    header="New Pepper-X App User installed", parameters={}):
-    url = "https://hooks.slack.com/services/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"
+    
+    channel_url_map = {"pim-apps-installations":"https://hooks.slack.com/services/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",
+    "pim-apps-enrichments":"https://hooks.slack.com/services/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",
+    "pim-apps-marketplaces":"https://hooks.slack.com/services/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",
+    "pim-apps-plugins":"https://hooks.slack.com/services/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",
+    }
+    url = channel_url_map[channel]
+
+    print(url)
 
     payload = {
         "channel": channel,
